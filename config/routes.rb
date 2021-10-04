52c6b5fa417855c2d79d2d4e989b9a3324be291f
gfdsvvgf <patrick.jj@outlook.jp>
2021-10-04 12:09:23 +0000

    フォロー機能追加完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   get "home/about" => "homes#about"
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end  
   resources :books  do  
     resources :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
