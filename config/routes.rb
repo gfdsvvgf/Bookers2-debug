124d9a6f7afcf211be39602d57d19ebbaab7fabd
gfdsvvgf <patrick.jj@outlook.jp>
2021-10-06 06:10:18 +0000

    検索機能追加完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   get "home/about" => "homes#about"
+  get '/search', to: 'searchs#search'
   resources :users,only: [:show,:index,:edit,:update] do
     resources :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
